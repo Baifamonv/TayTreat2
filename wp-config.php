2c4b433b0c2aebb14c042ba45313f93371d3f632
Jing <dskool@hotmail.com>
2016-07-20 13:54:20 -0400

    test configure change

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'taybase');
+define('DB_NAME', 'taybasetest');
 
 /** MySQL database username */
 define('DB_USER', 'taytreat');

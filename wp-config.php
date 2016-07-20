d06887e752395d2043a4b0173d750b2f5caaf304
Jing <dskool@hotmail.com>
2016-07-20 14:57:25 -0400

    test configure change

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', '	forge');
 define('DB_PASSWORD', 'TfLPcOoVmipHKu0OxbXd');
 
 /** MySQL hostname */
-define('DB_HOST', '10.131.10.176');
+define('DB_HOST', '139.59.190.211');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
